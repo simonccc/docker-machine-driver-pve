824d0f73f45fe204cd04a47eeb34b4297262665b
Lukas Kirylak <lukas@stellatarum.com>
2025-03-13 20:57:40 +0000

    feat: add '-version' flag
    Signed-off-by: Lukas Kirylak <lukas@stellatarum.com>

--- cmd/docker-machine-driver-pve/main.go
+++ cmd/docker-machine-driver-pve/main.go
@@ -1,10 +1,38 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+	"runtime/debug"
+
 	"github.com/rancher/machine/libmachine/drivers/plugin"
 	"github.com/stellatarum/docker-machine-driver-pve/cmd/docker-machine-driver-pve/driver"
 )
 
 func main() {
-	plugin.RegisterDriver(driver.NewDriver("", ""))
+	var (
+		showHelp    = flag.Bool("help", false, "Show help and exit")
+		showVersion = flag.Bool("version", false, "Show version information and exit")
+	)
+
+	flag.Parse()
+
+	switch {
+	case *showHelp:
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(0)
+	case *showVersion:
+		buildInfo, ok := debug.ReadBuildInfo()
+		if !ok {
+			fmt.Fprint(os.Stderr, "Build info is not available")
+			os.Exit(1)
+		}
+
+		fmt.Fprint(os.Stderr, buildInfo.Main.Version+"\n")
+		os.Exit(0)
+	default:
+		plugin.RegisterDriver(driver.NewDriver("", ""))
+	}
 }
